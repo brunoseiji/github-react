dc3632c30d8735fc8c70536890e8c7b4b51e28f2
Bruno Seiji <bruno_seiji_1995@hotmail.com>
2020-04-05 18:40:06 -0300

    Exibindo Issues

--- src/pages/Repositorio/style.js
+++ src/pages/Repositorio/style.js
@@ -49,3 +49,61 @@ export const Owner = styled.header `
         max-width: 400px;
     }
 `;
+
+export const IssuesList = styled.ul `
+    margin-top: 30px;
+    padding-top: 30px;
+    border-top: 1px solid #eee;
+    list-style: none;
+
+    li {
+        display: flex;
+        padding: 15px 10px;
+        align-items: center;
+
+        & + li {
+            margin-top: 12px;
+        }
+
+        img {
+            width: 36px;
+            height: 36px;
+            border-radius: 50%;
+        }
+
+        div {
+            flex: 1;
+            margin-left: 12px;
+
+            p {
+                margin-top: 10px;
+                font-size: 12px;
+                color: #000;
+            }
+        }
+
+        strong {
+            font-size: 15px;
+
+            a {
+                text-decoration: none;
+                color: #0D2636;
+                transition: .2s;
+
+                &:hover {
+                    color: #0071db;
+                }
+            }
+
+            span {
+                background: #222;
+                color: #fff;
+                border-radius: 4px;
+                font-size: 12px;
+                font-weight: 600;
+                padding: 4px 7px;
+                margin-left: 10px;
+            }
+        }
+    }
+`;
